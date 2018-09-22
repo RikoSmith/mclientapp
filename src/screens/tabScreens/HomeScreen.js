fb60c123f4d4ce714cc5e4d9990e34bd663ebc20
Riko <raimbek.riko@gmail.com>
2018-09-22 22:28:25 +0600

    some cards on home screen

--- src/screens/tabScreens/HomeScreen.js
+++ src/screens/tabScreens/HomeScreen.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+
+var width = Dimensions.get('window').width; //full width
+var height = Dimensions.get('window').height; //full height
 
 class HomeScreen extends Component {
   static navigationOptions = {
@@ -9,7 +13,30 @@ class HomeScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>HomeScreen</Text>
+        <Card title="Heartbeat" containerStyle={styles.card}>
+          <View>
+            <Image style={styles.image} resizeMode="cover" />
+            <Text style={styles.name}>Riko</Text>
+          </View>
+        </Card>
+        <Card title="Calories" containerStyle={styles.card}>
+          <View>
+            <Image style={styles.image} resizeMode="cover" />
+            <Text style={styles.name}>Riko</Text>
+          </View>
+        </Card>
+        <Card title="Some other info" containerStyle={styles.card}>
+          <View>
+            <Image style={styles.image} resizeMode="cover" />
+            <Text style={styles.name}>Riko</Text>
+          </View>
+        </Card>
+        <Card title="Another mHealth info" containerStyle={styles.card}>
+          <View>
+            <Image style={styles.image} resizeMode="cover" />
+            <Text style={styles.name}>Riko</Text>
+          </View>
+        </Card>
       </View>
     );
   }
@@ -22,5 +49,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  card: {
+    width: width - 10
   }
 });
